493c0637f41a031a7c85673adac8a6e8a3d4e09d
Antti Järvinen <antti.jarvinen@protacon.com>
2019-08-13 16:28:30 +0300

    Basic encrypting

--- SecretEncrypter/Program.cs
+++ SecretEncrypter/Program.cs
@@ -1,14 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Security.Cryptography;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 // ReSharper disable once IdentifierTypo
 namespace SecretEncrypter
 {
-    internal static class Program
+    public static class Program
     {
-        internal static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.Write("Path to the json file: ");
             var path = Console.ReadLine();
@@ -19,17 +21,29 @@ namespace SecretEncrypter
             var privateKey = cryptoServiceProvider.ExportParameters(true);
             var pubKey = cryptoServiceProvider.ExportParameters(false);
 
+            Console.WriteLine("Encrypting secrets...");
+
             JObject secrets = JObject.Parse(File.ReadAllText(Path.Combine(Environment.CurrentDirectory, path)));
 
+            var encryptedSecrets = new Dictionary<string, string>();
             foreach (var (key, value) in secrets)
             {
-                string valueString = value.First.ToObject<string>();
+                string valueString = value.ToObject<string>();
                 var bytesPlainTextData = System.Text.Encoding.Unicode.GetBytes(valueString);
-                var bytesCypherText = cryptoServiceProvider.Encrypt(bytesPlainTextData, false);
+                var bytesCypherText = cryptoServiceProvider.Encrypt(bytesPlainTextData, true);
                 var cypherText = Convert.ToBase64String(bytesCypherText);
-                Console.Write(cypherText);
+                encryptedSecrets.Add(key, cypherText);
             }
 
+            Console.WriteLine("Secrets encrypted. Writing json...");
+            using (StreamWriter file = File.CreateText(@"secrets.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, encryptedSecrets);
+            }
+            Console.WriteLine("Success!");
+
+            Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
         }
     }
